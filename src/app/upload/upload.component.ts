4f151136ab76177181737dfba2222759f5bfaa61
Pauliina Sapman <pauliips@ety-a2123b-29.edu.wks.metropolia.fi>
2018-02-05 13:32:26 +0200

    v3t2

---
+++ src/app/upload/upload.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Media} from '../models/media';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.scss']
+})
+export class UploadComponent implements OnInit {
+
+  file = new Media('');
+
+  formData = new FormData();
+
+  constructor() { }
+
+  setFile(evt) {
+    console.log(evt.target.files[0]);
+    const file: File = evt.target.files[0];
+    // lisää tiedosto formData-objektiin
+  }
+
+  uploadFile() {
+    // lisää tekstikenttien sisältö formData-objektiin
+    // lähetä tiedot
+  }
+
+  ngOnInit() {
+  }
+
+}
